748f83fb8e675d8162f1d592ff9d14ea94b51929
mlammon11 <mlammon11@gmail.com>
2024-10-18 21:41:19 -0400

    Starting Navbar
    - started working on navbar layout, added icons for each page

--- projectconnect/src/app/page.tsx
+++ projectconnect/src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Navbar from './components/navbar';
 
 export default function Home() {
   return (
-    <div>Hello Project Connect!</div>
+    <>
+      <Navbar></Navbar>
+      <div></div>
+    </>
+
   );
 }
