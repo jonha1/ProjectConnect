b38b56dcce13d130996d2a1a8a177e4c1467da48
mlammon11 <mlammon11@gmail.com>
2024-10-19 15:26:58 -0400

    Starting Searchbar
    - started the UI for searchbar, added filter and tag buttons

---
+++ projectconnect/src/app/components/searchbar.tsx
@@ -0,0 +1,12 @@
+import '../styles/searchbar.modules.css'
+export default function Searchbar() {
+  return (
+    <div className="parentContainer">
+        <div className="searchContainer">
+            <input id="search" type="text" placeholder="Search.."></input>
+            <button type="button" className="btn searchButtons">Filter</button>
+            <button type="button" className="btn searchButtons">Tags</button>
+        </div>
+    </div>
+  );
+}
--- projectconnect/src/app/page.tsx
+++ projectconnect/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from './components/navbar';
+import Searchbar from './components/searchbar';
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
-      <div></div>
+      <Searchbar></Searchbar>
     </>
 
   );
