ae84eb8af5e849317fe523ac43df585d7be86b45
mlammon11 <mlammon11@gmail.com>
2024-10-24 17:35:21 -0400

    Setting up Login Page
    - set up UI for login page with title, description, inputs for username and password, signup/signin buttons

---
+++ projectconnect/src/app/login/page.tsx
@@ -0,0 +1,24 @@
+import "../styles/login.page.css";
+
+export default function Login() {
+  return (
+    <>
+      <div className="loginContainer">
+        <div id="title">ProjectConnect</div>
+        <div id="description">Login In</div>
+        <input id="search" type="text" placeholder="Username or Email"></input>
+        <input id="search" type="password" placeholder="Password"></input>
+        <div className="buttonContainer">
+          <button type="button" className="btn registerButtons">
+            <a aria-current="page" href="/">
+              Sign In
+            </a>
+          </button>
+          <button type="button" className="btn registerButtons">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
