ae02175dc95520807ba76f143242da6e1e0af537
mlammon11 <mlammon11@gmail.com>
2024-10-29 21:56:52 -0400

    Account Page Layout
    - created the layout for the account page with some temporary "data"
    - need to create the project cards component next

--- projectconnect/src/app/components/searchbar.tsx
+++ projectconnect/src/app/components/searchbar.tsx
@@ -3,7 +3,7 @@ export default function Searchbar() {
   return (
     <div className="parentContainer">
         <div className="searchContainer">
-            <input id="search" type="text" placeholder="Search.."></input>
+            <input id="search" type="text" placeholder="Search..."></input>
             <button type="button" className="btn searchButtons">Filter</button>
             <button type="button" className="btn searchButtons">Tags</button>
         </div>
