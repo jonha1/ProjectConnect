c1f43584d6eb80daf709ee61479f6bb534c44419
mlammon11 <mlammon11@gmail.com>
2024-11-01 19:26:07 -0400

    Web sessions 1
    Using next.js + supabase to create web sessions

--- projectconnect/src/app/pages/home/page.tsx
+++ projectconnect/src/app/pages/home/page.tsx
@@ -1,9 +1,34 @@
-import Navbar from '../../components/navbar';
-import Searchbar from '../../components/searchbar';
-import HomepageCards from '../../components/homepage_cards';
+import { supabase } from "../../lib/supabase";
+
+import Navbar from "../../components/navbar";
+import Searchbar from "../../components/searchbar";
+import HomepageCards from "../../components/homepage_cards";
 
 export default function Home() {
-  const tags = ["Arts/Crafts", "Business", "Coding", "Engineering", "Math", "Music", "Science", "Writing", "Other"];
+  const setNewView = async () => {
+    const { data, error } = await supabase
+    .from("views")
+    .insert({
+      name: 'random name'
+    })
+
+    if(data) console.log(data)
+    if(error) console.log(error)
+  };
+
+  setNewView();
+
+  const tags = [
+    "Arts/Crafts",
+    "Business",
+    "Coding",
+    "Engineering",
+    "Math",
+    "Music",
+    "Science",
+    "Writing",
+    "Other",
+  ];
 
   return (
     <>
