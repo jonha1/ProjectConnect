39e98962d7bfa3ec50d15e014924a0c19b0513fa
Alice Leung <aliceleung34@gmail.com>
2024-11-02 14:49:20 -0400

    changed css and testing dropdown

--- projectconnect/src/app/pages/addproject/page.tsx
+++ projectconnect/src/app/pages/addproject/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Navbar from '../../components/navbar';
 import "../../styles/newproject.css";
 
@@ -5,34 +6,70 @@ export default function Createpost() {
   return (
     <>
       <Navbar />
+   
       <h1 className="formHeader">Create Project</h1>
+      <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Select Tag
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <li><a className="dropdown-item" href="#">Arts/Crafts</a></li>
+            <li><a className="dropdown-item" href="#">Business</a></li>
+            <li><a className="dropdown-item" href="#">Coding</a></li>
+            <li><a className="dropdown-item" href="#">Engineering</a></li>
+            <li><a className="dropdown-item" href="#">Math</a></li>
+            <li><a className="dropdown-item" href="#">Music</a></li>
+            <li><a className="dropdown-item" href="#">Science</a></li>
+            <li><a className="dropdown-item" href="#">Writing</a></li>
+            <li><a className="dropdown-item" href="#">Other</a></li>
+          </ul>
+        </div>
+
+
+        <div className="dropdown show">
+  <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown link
+  </a>
+
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a className="dropdown-item" href="#">Action</a>
+    <a className="dropdown-item" href="#">Another action</a>
+    <a className="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
 
       <div className="formContainer">
-
-        <div className="formDropdown">
-          <select name="Tags" id="Tags" className="formDropdown">
-            <option value="Arts/Crafts">Arts/Crafts</option>
-            <option value="Business">Business</option>
-            <option value="Coding">Coding</option>
-            <option value="Engineering">Engineering</option>
-            <option value="Math">Math</option>
-            <option value="Music">Music</option>
-            <option value="Science">Science</option>
-            <option value="Writing">Writing</option>
-            <option value="Other">Other</option>
-          </select>
+        
+        <div className='formHeader'>
+          <h3> Everyone</h3>
         </div>
 
         <form className="formInput">
-          <input type="text" id="title" placeholder="Title" className="inputBox" required />
-          <input type="text" id="description" placeholder="Description" className="inputBox" />
+          <input type="text" id="title" placeholder="Title*" className="inputBox" required />
+          <textarea id="description" placeholder="Description*" className="inputBox" required></textarea>
           <input type="text" id="links" placeholder="Links" className="inputBox" />
           <input type="text" id="contact" placeholder="Contact Information" className="inputBox" />
         </form>
 
-        <button type="submit" className="submit-button">Post</button>
+        <div className='formHeader'>
+            <h3> Members</h3>
+        </div>
 
+        <form className='formInput'>
+          <textarea id="more-description" placeholder="More Description*" className="inputBox" required></textarea>
+          <textarea id="more-links" placeholder="More Links" className="inputBox" />
+          <textarea id="more-contact" placeholder="More Contact Information" className="inputBox" />
+        </form>
+
+        <button type="submit" className="submit-button">Post</button>
       </div>
     </>
   );
 }
+
