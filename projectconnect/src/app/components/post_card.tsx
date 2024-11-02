da9a64891baf9cfb3f67818dfbd774e9cd6fb216
William Li <liwilliam100@gmail.com>
2024-11-02 14:29:27 -0400

    fix: Post Cards UI
    Changes the appearance of the post cards as well as made them selectable routing to new page

--- projectconnect/src/app/components/post_card.tsx
+++ projectconnect/src/app/components/post_card.tsx
@@ -1,9 +1,25 @@
+"use client"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
+import {
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from '../styles/post_card.module.css';
 
 export default function PostCard({ postName, postInfo, creatorName }) {
+  const router = useRouter();
+  const handleClickPost = () => {
+    router.push("/");
+  }
+
   return (
-    <div className={styles.cardContainer}>
-      <h2 className={styles.postName}>{postName}</h2>
+    <div className={styles.cardContainer} onClick = {handleClickPost}>
+      <div className={styles.header}>
+        <h2 className={styles.postName}>{postName}</h2>
+        <a className={styles.iconLink}>
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+        </a>
+      </div>
       <p className={styles.postInfo}>{postInfo}</p>
       <p className={styles.creatorName}>Created by: {creatorName}</p>
     </div>
