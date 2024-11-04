4cd87d339f052029b152479381d73b41d41ea05d
Alice Leung <aliceleung34@gmail.com>
2024-11-04 11:40:11 -0500

    Fixed dropdown button issue and added CSS styling(padding)

--- projectconnect/src/app/pages/addproject/page.tsx
+++ projectconnect/src/app/pages/addproject/page.tsx
@@ -6,46 +6,28 @@ export default function Createpost() {
   return (
     <>
       <Navbar />
-   
+
       <h1 className="formHeader">Create Project</h1>
-      <div className="dropdown">
-          <button
-            className="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Select Tag
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <li><a className="dropdown-item" href="#">Arts/Crafts</a></li>
-            <li><a className="dropdown-item" href="#">Business</a></li>
-            <li><a className="dropdown-item" href="#">Coding</a></li>
-            <li><a className="dropdown-item" href="#">Engineering</a></li>
-            <li><a className="dropdown-item" href="#">Math</a></li>
-            <li><a className="dropdown-item" href="#">Music</a></li>
-            <li><a className="dropdown-item" href="#">Science</a></li>
-            <li><a className="dropdown-item" href="#">Writing</a></li>
-            <li><a className="dropdown-item" href="#">Other</a></li>
-          </ul>
+
+
+      <div className="dropdown show">
+        <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Project Tags
+        </a>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <a className="dropdown-item" href="#">Arts/Crafts</a>
+          <a className="dropdown-item" href="#">Business</a>
+          <a className="dropdown-item" href="#">Coding</a>
+          <a className="dropdown-item" href="#">Engineering</a>
+          <a className="dropdown-item" href="#">Math</a>
+          <a className="dropdown-item" href="#">Music</a>
+          <a className="dropdown-item" href="#">Science</a>
+          <a className="dropdown-item" href="#">Writing</a>
+          <a className="dropdown-item" href="#">Other</a>
         </div>
-
-
-        <div className="dropdown show">
-  <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown link
-  </a>
-
-  <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a className="dropdown-item" href="#">Action</a>
-    <a className="dropdown-item" href="#">Another action</a>
-    <a className="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
+      </div>
 
       <div className="formContainer">
-        
         <div className='formHeader'>
           <h3> Everyone</h3>
         </div>
@@ -58,7 +40,7 @@ export default function Createpost() {
         </form>
 
         <div className='formHeader'>
-            <h3> Members</h3>
+          <h3> Members</h3>
         </div>
 
         <form className='formInput'>
@@ -69,6 +51,9 @@ export default function Createpost() {
 
         <button type="submit" className="submit-button">Post</button>
       </div>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </>
   );
 }
