3b3d601b087c0d9b707c873502b7382102eb2ce2
Jonathan Ha <78988736+jonha1@users.noreply.github.com>
2024-11-05 13:24:37 -0500

    react linting finished

---
+++ projectconnect/eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tsEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tsEslint,
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tsEslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
