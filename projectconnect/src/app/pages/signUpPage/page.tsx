9bed2736fc10ac1043a819154b4b9bb5a22afcae
Alice Leung <aliceleung34@gmail.com>
2024-11-09 22:32:12 -0500

    Basic Skeleton for SignUpPage with basic css and tsx

---
+++ projectconnect/src/app/pages/signUpPage/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { supabase } from "../../lib/supabase";
+import "../../styles/signUp.page.css";
+import { useState } from "react";
+
+export default function Login() {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const router = useRouter();
+
+  const signin = async () => {
+    try {
+      const { data: loginData, error } = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password,
+      });
+
+      if (loginData) {
+        console.log("Logged in:", loginData);
+        router.refresh();
+        router.push("/");
+      }
+
+      if (error) {
+        console.error("Login error:", error);
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
+  const signup = async () => {
+    try {
+      const { data: signUpData, error } = await supabase.auth.signUp({
+        email: data.email,
+        password: data.password,
+      });
+
+      if (signUpData) {
+        console.log("Sign up successful:", signUpData);
+      }
+
+      if (error) {
+        console.error("Sign up error:", error);
+      }
+    } catch (error) {
+      console.error("Sign up error:", error);
+    }
+  };
+
+  const handleChange = (e: any) => {
+    const{name, value} = e.target;
+    setData((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <>
+      <div className="loginContainer">
+        <div id="title">ProjectConnect</div>
+        <div id="description">Sign Up</div>
+        <input id="email" type="text" name="email" placeholder="Email" value={data?.email} onChange={handleChange}></input>
+        <input id="password" type="password" name="password" placeholder="Password" value={data?.password} onChange={handleChange}></input>
+        <div className="buttonContainer">
+          <button type="button" className="btn registerButtons" onClick={signup}>
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </>
+  );
+
+}
