9c713a798f868f77798f7c5fa5c32357c88552c7
mlammon11 <mlammon11@gmail.com>
2024-11-18 13:50:20 -0500

    Updating Port
    changed port from 5000 to 5001

--- projectconnect/backend/project_flask/app.py
+++ projectconnect/backend/project_flask/app.py
@@ -179,4 +179,4 @@ def get_About_me():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
--- projectconnect/src/app/pages/login/page.tsx
+++ projectconnect/src/app/pages/login/page.tsx
@@ -19,7 +19,7 @@ export default function Login() {
 
   const signin = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/login', {
+      const response = await fetch('http://127.0.0.1:5001/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
