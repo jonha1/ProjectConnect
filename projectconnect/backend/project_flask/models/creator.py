4707897b215b4660f22d5e226b88c9b1be1de4ba
Jonathan Ha <78988736+jonha1@users.noreply.github.com>
2024-12-01 19:59:54 -0500

    changed to relative import

--- projectconnect/backend/project_flask/models/creator.py
+++ projectconnect/backend/project_flask/models/creator.py
@@ -1,8 +1,8 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from project_flask.models.member import Member
-from project_flask.models.project import Project
+from models.member import Member
+from models.project import Project
 
 
 class Creator(Member):
